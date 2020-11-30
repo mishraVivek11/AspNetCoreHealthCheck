579b83c952e9d1abdd0337560ab3bb261b6e5c9f
Vivek Mishra <vivek.mishra04@yahoo.com>
2020-12-01 02:26:42 +0530

    Added custom certificate healthcheck,JSON healthCheck response

--- AspNetCoreHealthCheck/Startup.cs
+++ AspNetCoreHealthCheck/Startup.cs
@@ -13,40 +13,55 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
 namespace AspNetCoreHealthCheck
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IWebHostEnvironment webHostEnvironment)
         {
             Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            ConfigureHealthChecks(services);
+        }
+
+        private void ConfigureHealthChecks(IServiceCollection services)
+        {
             string connectionString = this.Configuration["ConnectionStrings:DefaultConnection"];
             string domainServiceUrl = this.Configuration["HealthChecks:DomainServiceUrl"];
+            string certificatePath = this.Configuration["HealthChecks:CertificatePath"];
 
-            services.AddControllers();
             services.AddHealthChecks()
-                .AddSqlServer(
-                connectionString,name:"DB Health Check",
-                failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy, 
+               .AddSqlServer(
+               connectionString, name: "DB Health Check",
+               failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy,
+               tags: new string[] { "ready" },
+               timeout: new TimeSpan(0, 0, 2)
+               )
+               .AddUrlGroup(
+               new Uri($"{domainServiceUrl}"),
+               "Domain Service Health Check",
+               failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded,
                 tags: new string[] { "ready" },
-                timeout: new TimeSpan(0, 0, 2)
-                )
-                .AddUrlGroup(
-                new Uri($"{domainServiceUrl}"),
-                "Domain Service Health Check",
-                failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded,
-                 tags: new string[] { "ready" },
-                timeout: new TimeSpan(0, 0, 2)
-                );
-            
+               timeout: new TimeSpan(0, 0, 2)
+               )
+               //Custom health check for certificate file
+               .AddServiceCertificateCheck(
+                WebHostEnvironment.ContentRootPath+ certificatePath,
+                healthStatus: HealthStatus.Unhealthy,
+               tags: new string[] { "ready" },
+               timeout: new TimeSpan(0, 0, 2)
+               );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,7 +87,24 @@ namespace AspNetCoreHealthCheck
                     [HealthStatus.Degraded]=StatusCodes.Status500InternalServerError,
                     [HealthStatus.Unhealthy]=StatusCodes.Status503ServiceUnavailable
                     },
-                    AllowCachingResponses=false
+                    ResponseWriter = (HttpContext context, HealthReport rpt) => {
+
+                        context.Response.ContentType = "application/json";
+
+                        var json = new JObject
+                            (
+                                new JProperty("Status", rpt.Status.ToString()),
+                                new JProperty("CheckDuration", rpt.TotalDuration.TotalSeconds.ToString("0.000")),
+                                new JProperty("Details", new JObject(rpt.Entries.Select(dictItem =>
+                             new JProperty(dictItem.Key, new JObject(
+                                 new JProperty("Status", dictItem.Value.Status.ToString()),
+                                 new JProperty("CheckDuration", dictItem.Value.Duration.TotalSeconds.ToString("0.000"))
+                                 ))
+                         )))
+                            );
+                        return context.Response.WriteAsync(json.ToString(Newtonsoft.Json.Formatting.Indented));
+                    },
+                    AllowCachingResponses =false
                 });
             });
         }
