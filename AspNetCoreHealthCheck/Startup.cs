2f500bc3ce99660f46d4c7f98641860e56b4fe17
Vivek Mishra <vivek.mishra04@yahoo.com>
2020-12-01 01:36:08 +0530

    Added health check for dependant service

--- AspNetCoreHealthCheck/Startup.cs
+++ AspNetCoreHealthCheck/Startup.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -26,14 +29,24 @@ namespace AspNetCoreHealthCheck
         public void ConfigureServices(IServiceCollection services)
         {
             string connectionString = this.Configuration["ConnectionStrings:DefaultConnection"];
+            string domainServiceUrl = this.Configuration["HealthChecks:DomainServiceUrl"];
 
             services.AddControllers();
-            services.AddHealthChecks().AddSqlServer(
-                connectionString,name:"DB health Check",
+            services.AddHealthChecks()
+                .AddSqlServer(
+                connectionString,name:"DB Health Check",
                 failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy, 
                 tags: new string[] { "ready" },
                 timeout: new TimeSpan(0, 0, 2)
+                )
+                .AddUrlGroup(
+                new Uri($"{domainServiceUrl}"),
+                "Domain Service Health Check",
+                failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded,
+                 tags: new string[] { "ready" },
+                timeout: new TimeSpan(0, 0, 2)
                 );
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +66,14 @@ namespace AspNetCoreHealthCheck
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health");
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions() {
+                    ResultStatusCodes = {
+                    [HealthStatus.Healthy]=StatusCodes.Status200OK,
+                    [HealthStatus.Degraded]=StatusCodes.Status500InternalServerError,
+                    [HealthStatus.Unhealthy]=StatusCodes.Status503ServiceUnavailable
+                    },
+                    AllowCachingResponses=false
+                });
             });
         }
     }
